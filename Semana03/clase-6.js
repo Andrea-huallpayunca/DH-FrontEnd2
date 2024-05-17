ae3ffc3f86fd45f941b6e71ed7f92b41ab3e7c35
Andrea-huallpayunca <andreahuallpayuncah@gmail.com>
2024-05-16 20:58:30 -0500

    Eliminar album

--- Semana03/clase-6.js
+++ Semana03/clase-6.js
@@ -1,22 +1,60 @@
 /* -------------------------------------------------------------------------- */
 /*                        [4] FUNCION: marcar favorito                        */
 /* -------------------------------------------------------------------------- */
-// - buscar el album por id en el array 
+// - buscar el album por id en el array
 // - cambiar el estado del like
 // - volver a renderizar
 function marcarFavorito() {
-    // Debemos seleccionar todos los botones de like
-    
+  // Debemos seleccionar todos los botones de like
+  // <i id="x123" class="fa fa-heart favorito"></i> // este es nuestro model de la etiqueta i
+  const botonesDeLike = document.querySelectorAll(".fa-heart");
+  // console.log(botonesDeLike); // la lista de nodos esta correctamente capturada
+
+  botonesDeLike.forEach((boton) => {
+    boton.addEventListener("click", function (evento) {
+      console.log(evento);
+      console.log(evento.target);
+      // console.log(evento.target.classList);
+      console.log(evento.target.id); // este es el atributo clave que me sirve para buscar en el array de albmuesFamosos
+      let albumId = evento.target.id;
+
+      // paso siguiente ir a buscar en el array para cambiar el estado de album.like
+      albumesFamosos.forEach((album) => {
+        if (album.id == albumId) {
+          console.log(
+            `Coinciden los álbumes ${albumId} es igual al ${album.id}`
+          );
+          console.log(album.like);
+
+          // nos queda cambiar la propiedad album.like al estado contrario del actual
+          // if (album.like == false) {
+          //     album.like = true
+          // } else {
+          //     album.like = false
+          // }
+
+          // refactorizamos el condicional anterior para que quede mas elegante en una sola linea
+          album.like = !album.like;
+          console.log(`cambiamos la propiedad a ${album.like}`);
+        }
+      });
+
+      // Renderizar (pintar) nuevamente las tarjetas para que se pinte los like de los álbumes
+      renderizarAlbumes(albumesFamosos); // pintamos nuevamente todas dlas tarjetas con la info actualizada de albumesFamosos
+      mostrarDatosEnPerfil(albumesFamosos); // es necesario actualizar los datos de mi playlist
+
+      // Recursividad: para agreagar nuevamente el listener para seguir ~escuchado el eveto de los botones cada vez que apriete cualquiera de ellos
+      marcarFavorito(); // es decir que agrego nuevamente los eventos a los botones renderizados en la linea 41
+    });
+  });
 }
-marcarFavorito()
-
-
+marcarFavorito();
 
 /* ----------------------------- MESA DE TRABAJO ---------------------------- */
 /* -------------------------------------------------------------------------- */
 /*                         [5] FUNCION: Eliminar album                        */
 /* -------------------------------------------------------------------------- */
-// Debemos desarrollar la funcion de eliminar un album. Para esto le vamos a 
+// Debemos desarrollar la funcion de eliminar un album. Para esto le vamos a
 // preguntar al usuario cual quiere eliminar.
 // Vamos a seguir las indicaciones que nos permiten lograrlo utilizando eventos.
 // 1- Hay que escuchar el evento de 'keydown' para detectar cuando el usuario presiona la tecla f ✅
@@ -25,18 +63,40 @@ marcarFavorito()
 // 4- Si la busqueda nos da un resultado válido, procedemos a borrar el objeto del array
 // 5- Acto seguido debemos llamar a las funciones de renderizar y marcar favorito para que sean nuevamente aplicadas.
 
-
-window.addEventListener("keydown", function(evento) {
-    // console.log(evento.key);
-    // console.log(evento.code);
-    eliminarAlbum(evento)
-})
-
-// window.addEventListener("keydown", eliminarAlbum)
-
-
-function eliminarAlbum(e) {
+//Paso 1
+window.addEventListener("keydown", function (event) {
+  // keydown todas las teclas del teclado
+  //   console.log(event);
+  //   console.log(event.key);
+  //   console.log(event.code);
+  const letraPresionada = event.code;
+  if (letraPresionada === "KeyF") {
+    // Paso 2
+    const albúmParaEliminar = prompt("¿Qué albúm desea eliminar?");
     
-}
+    // Paso 3
+    const posiciónAlbum = albumesFamosos.findIndex(
+      (element) => element.nombre === albúmParaEliminar
+    );
+    
+    // Paso 4
+    if (posiciónAlbum != -1) {
+      albumesFamosos.splice(posiciónAlbum, 1);
+      // Paso 5
+      renderizarAlbumes(albumesFamosos);
+      marcarFavorito();
+    } else {
+      alert("El nombre del álbum ingresado no existe.");
+    }
+  }
+});
 
-eliminarAlbum()
+// window.addEventListener("keypress", eliminarAlbum); // keypress solo toma teclas alfanumericas
+
+// function eliminarAlbum(event) {
+//   console.log(event);
+//   console.log(event.key);
+//   console.log(event.code);
+// }
+
+// eliminarAlbum();
